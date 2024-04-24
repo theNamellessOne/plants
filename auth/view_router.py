82309c6bf3ee09a9e6002ecd2d7baa5f7a693b63
theNamellessONe <ighor.pavluuk@gmail.com>
2024-04-24 17:12:11 +0300

    added register form

--- auth/view_router.py
+++ auth/view_router.py
@@ -4,7 +4,8 @@ from starlette.templating import Jinja2Templates
 
 from auth.auth_service import AuthService, get_auth_service, get_principal
 from auth.jwt_service import get_jwt_service, JwtService
-from user.schema import UserLogin
+from user.schema import UserLogin, UserRegister
+from user.service import UserService, get_user_service
 
 router = APIRouter()
 templates = Jinja2Templates(directory="templates")
@@ -50,6 +51,39 @@ def try_login(request: Request,
         return templates.TemplateResponse("auth/auth_error.html", {
             "request": request,
             "detail": "Wrong email or password"
+        })
+
+    token = jwt_service.create_access_token(
+        subject=result.id,
+        payload={
+            "id": result.id,
+            "email": result.email,
+            "username": result.username,
+            "is_admin": result.is_admin,
+        }
+    )
+    response.set_cookie(key="Authorization", value=f"{token}", httponly=False)
+    response.headers["HX-Redirect"] = "/plants"
+    return {}
+
+
+@router.post("/try-register")
+def try_register(entity: UserRegister,
+                 request: Request,
+                 response: Response,
+                 auth_service: AuthService = Depends(get_auth_service),
+                 user_service: UserService = Depends(get_user_service),
+                 jwt_service: JwtService = Depends(get_jwt_service),
+                 principal=Depends(get_principal)):
+    if principal:
+        response.headers["HX-Redirect"] = "/plants"
+        return {}
+
+    result = user_service.create(entity)
+    if not result:
+        return templates.TemplateResponse("auth/auth_error.html", {
+            "request": request,
+            "detail": "Could not register"
         })
 
     token = jwt_service.create_access_token(
